ea1d77613d18dd8d024281df0c7efbe8bc1fda65
phuong.nguyen.gian <Nextop@123>
2019-10-21 17:09:01 +0700

    Helper template

---
+++ app/helpers/rental-property-type.js
@@ -0,0 +1,15 @@
+import { helper } from '@ember/component/helper';
+
+const communityPropertyTypes = [
+  'Codon',
+  'Townhouse',
+  'Apartment'
+];
+
+export function rentalPropertyType([propertyType]){
+  if(communityPropertyTypes.includes(propertyType)){
+    return 'Community';
+  }
+  return 'Standalone';
+}
+export default helper(rentalPropertyType);
